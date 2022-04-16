180d17999011bb504211e10e196552f064d7efac
Najib Fikri <najibfikri13@gmail.com>
2022-04-16 10:54:21 +0700

    Add source

---
+++ index.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebForm.index" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title></title>
+    <style>
+        * {
+            text-transform: capitalize;
+        }
+        .auto-style1 {
+            height: 35px;
+        }
+    </style>
+</head>
+<body>
+    <h1>sistem informasi</h1>
+    <form id="form" runat="server">
+        <table>
+            <tr>
+                <td class="auto-style1">
+                    <h3>data mahasiswa</h3>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnSelect" runat="server" Text="Select All Data" OnClick="btnSelect_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:GridView ID="selectAllData" runat="server"></asp:GridView>
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td colspan="2">
+                    <h3>tambah dan ubah data</h3>
+                    <br />
+                </td>
+            </tr>
+            <tr>
+                <td>id</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="intId" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>firstname</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtFirstname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>lastname</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtLastname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>username</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>gender</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="charGender" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>email</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="btnInsertion" runat="server" Text="Tambah" OnClick="btnInsertion_Click" Width="109px" />
+                    <asp:Button ID="btnUpdate" runat="server" Text="Ubah" OnClick="btnUpdate_Click" Width="109px" />
+                    <asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td colspan="3">
+                    <h3>hapus data</h3>
+                </td>
+            </tr>
+            <tr>
+                <td>mahasiswa ID</td>
+                <td>:</td>
+                <td>
+                    <asp:TextBox ID="txtEmployeeID" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <asp:Button ID="btnDelete" runat="server" Text="Hapus" OnClick="btnDelete_Click" />
+                    <asp:Label ID="lblmessage" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </form>
+
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Npgsql;
+using System.Configuration;
+using System.Data;
+
+namespace WebForm
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=PBO;User Id=postgres;password=nova");
+                connection.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "Select * from mhs";
+                cmd.CommandType = CommandType.Text;
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                cmd.Dispose();
+                connection.Close();
+
+                selectAllData.DataSource = dt;
+                selectAllData.DataBind();
+            }
+            catch (Exception ex) {}
+        }
+
+        protected void btnInsertion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=PBO;User Id=postgres;password=nova");
+                connection.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "Insert into mhs (id_user, first_name, last_name,username,gender,email,password) values (@id,@first_name,@last_name,@username,@gender,@email,@password)";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new NpgsqlParameter("@id", Convert.ToInt32(intId.Text)));
+                cmd.Parameters.Add(new NpgsqlParameter("@first_name", txtFirstname.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@last_name", txtLastname.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@username", txtUsername.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@gender", charGender.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@email", txtEmail.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@password", txtPassword.Text));
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                connection.Close();
+                intId.Text = "";
+                txtFirstname.Text = "";
+                txtLastname.Text = "";
+                txtUsername.Text = "";
+                charGender.Text = "";
+                txtEmail.Text = "";
+                txtPassword.Text = "";
+                lblmsg.Text = "Data mahasiswa berhasil ditambahkan";
+            }
+            catch (Exception ex) { }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=PBO;User Id=postgres;password=nova");
+                connection.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "update mhs set first_name=@first_name,last_name=@last_name,username=@username,gender=@gender,email=@email,password=@password where id_user=@id";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new NpgsqlParameter("@id", Convert.ToInt32(intId.Text)));
+                cmd.Parameters.Add(new NpgsqlParameter("@first_name", txtFirstname.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@last_name", txtLastname.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@username", txtUsername.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@gender", charGender.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@email", txtEmail.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@password", txtPassword.Text));
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                connection.Close();
+                intId.Text = "";
+                txtFirstname.Text = "";
+                txtLastname.Text = "";
+                txtUsername.Text = "";
+                charGender.Text = "";
+                txtEmail.Text = "";
+                txtPassword.Text = "";
+                lblmsg.Text = "Data mahasiswa berhasil diubah";
+            }
+            catch (Exception ex) { }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try /* Deletion After Validations*/
+            {
+                NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=PBO;User Id=postgres;password=nova");
+                connection.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "Delete from mhs where id_user=@id";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new NpgsqlParameter("@id", Convert.ToInt32(txtEmployeeID.Text)));
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                connection.Close();
+                txtEmployeeID.Text = "";
+                lblmessage.Text = "Data mahasiswa berhasil dihapus";
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
